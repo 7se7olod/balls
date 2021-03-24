acd4f5dd2593247af0f12c57c5c1cb04916099ae
7se7olod <pushin_95@mail.ru>
2021-03-24 12:00:20 +0400

    qw

--- Balls.playground/Sources/Balls.swift
+++ Balls.playground/Sources/Balls.swift
@@ -10,10 +10,25 @@ public init(colors: [UIColor]) {
     self.colors = colors
     super.init(frame: CGRect(x:0, y:0, width: 400, height: 400))
     backgroundColor = UIColor.gray
+    ballsView()
 }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-}
 
+
+private var ballSize: CGSize = CGSize(width: 40, height: 40)
+
+func ballsView() {
+    for (index, color) in colors.enumerated() {
+        let ball = UIView(frame: CGRect.zero)
+        ball.backgroundColor = color
+        addSubview(ball)
+        balls.append(ball)
+        let origin = 40*index + 100
+        ball.frame = CGRect(x: origin, y: origin, width: Int(ballSize.width), height: Int((ballSize.height)))
+        ball.layer.cornerRadius = ball.bounds.width / 2.0
+    }
+}
+}
